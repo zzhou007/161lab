50b15eb99aa43749398a1f93c97e1119492d1a81
Zi Zhou <zzhou007@ucr.edu>
2016-01-20 16:43:42 -0800

    Update my_alu.vhd

--- my_alu.vhd
+++ my_alu.vhd
@@ -14,3 +14,42 @@ generic(NUMBITS : natural := 32);
            zero : out  STD_LOGIC);
 end my_alu;
 
+signal Aout : std_logic_vector(NUMBITS -1 downto 0);
+signal Bout : std_logic_vector(NUMBITS -1 downto 0);
+signal Rout : std_logic_vector(NUMBITS -1 downto 0);
+
+architecture structural of my_alu is
+	
+	bcd_binA:bcd_bin
+	port map (
+		I => A;
+		opcode => opcode;
+		O => Ain;
+	);
+	
+	bcd_binB:bcd_bin
+	port map (
+		I => B;
+		opcode => opcode;
+		O => Bin;
+	);
+	
+	alu:bin_alu
+	port map (
+		A => Ain;
+		B => Bin;
+		opcode => opcode;
+		overflow => overflow;
+		carryout => carryout;
+		zero => zero;
+		result => Rout;
+	);
+	
+	bin_bcdOut:bin_bcd
+	port map (
+		I => Rout;
+		opcode => opcode;
+		O => result;
+	);
+	
+end architecture structural;
