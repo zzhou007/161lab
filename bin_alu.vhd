3a5d86c697ef407ed936e8c290bf310849747585
Zi Zhou <zzhou007@ucr.edu>
2016-01-20 16:44:17 -0800

    Update bin_alu.vhd

--- bin_alu.vhd
+++ bin_alu.vhd
@@ -8,7 +8,7 @@ generic(NUMBITS : natural := 32);
     Port ( A : in  STD_LOGIC_VECTOR(NUMBITS - 1 downto 0);	
            B : in  STD_LOGIC_VECTOR(NUMBITS - 1 downto 0);
            opcode : in  STD_LOGIC_VECTOR(3 downto 0);
-           result : out  STD_LOGIC_VECTOR(NUMBITS + 3 downto 0);
+           result : out  STD_LOGIC_VECTOR(NUMBITS - 1 downto 0);
            carryout : out  STD_LOGIC;
            overflow : out  STD_LOGIC;
            zero : out  STD_LOGIC);
