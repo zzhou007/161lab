52d11f9ba7469bc38d6f65f20ee5e944fd5004a8
Zi Zhou <zzhou007@ucr.edu>
2016-01-20 16:44:31 -0800

    Update bin_bcd.vhd

--- bin_bcd.vhd
+++ bin_bcd.vhd
@@ -6,6 +6,7 @@ use IEEE.NUMERIC_STD.ALL;
 entity bin_bcd is
 generic(NUMBITS : natural := 32);
     Port ( I : in  STD_LOGIC_VECTOR(NUMBITS - 1 downto 0);	
+			  opcode : in STD_LOGIC_VECTOR (3 downto 0);
            O : out  STD_LOGIC_VECTOR(NUMBITS - 1 downto 0);
 end bcd_bin;
 
