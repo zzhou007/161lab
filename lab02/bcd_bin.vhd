9a165883ae6fac173e4a68587db807ccdd8e51ed
Zi Zhou <zzhou007@ucr.edu>
2016-01-29 13:29:36 -0800

    Update bcd_bin.vhd

--- lab02/bcd_bin.vhd
+++ lab02/bcd_bin.vhd
@@ -2,6 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use ieee.numeric_std.all;
+use ieee.std_logic_arith.conv_std_logic_vector;
 library work;
 
 entity bcd_bin is
@@ -21,10 +23,10 @@ architecture Behavioral of bcd_bin is
 	signal T5: std_logic_vector (3 downto 0);
 	signal T6: std_logic_vector (3 downto 0);
 	signal T7: std_logic_vector (3 downto 0);
-	signal Tint: integer;
+		
 
 	begin
-
+	--moving each digit of bcd input into own section
 		T0 <= I(3 downto 0);
 		T1 <= I(7 downto 4);
 		T2 <= I(11 downto 8);
@@ -34,43 +36,37 @@ architecture Behavioral of bcd_bin is
 		T6 <= I(27 downto 24);
 		T7 <= I(31 downto 28);
 
-		--ALU 
-		process (I, T0, T1, T2, T3, T4, T5, T6, T7, Tint)
+		process (I, T0, T1, T2, T3, T4, T5, T6, T7)
+		variable Tint : integer;
 		begin
+			--stores the bin when adding all the sections together
+			Tint := 0; 
+			
 			--if unsigned
 			if (opcode = "1000" or opcode = "1001") then
-				Tint <= (to_integer(unsigned(T0)) + to_integer(unsigned(T1) * 10) +
-							to_integer(unsigned(T2) * 100) + to_integer(unsigned(T3) * 1000) +
-							to_integer(unsigned(T4) * 10000) + to_integer(unsigned(T5) * 100000) +
-							to_integer(unsigned(T6) * 1000000) + to_integer(unsigned(T7) * 10000000));
-				O <= std_logic_vector(to_unsigned(Tint, NUMBITS));
+				Tint := (to_integer(unsigned(T0)) + to_integer(unsigned(T1)) * 10 +
+							to_integer(unsigned(T2)) * 100 + to_integer(unsigned(T3)) * 1000 +
+							to_integer(unsigned(T4)) * 10000 + to_integer(unsigned(T5)) * 100000 +
+							to_integer(unsigned(T6)) * 1000000 + to_integer(unsigned(T7)) * 10000000);
+				O <= conv_std_logic_vector(Tint, NUMBITS);
 				
 			--if signed
 			else 
 				--if pos
 				if T7 = "0000" then
-					Tint <= (to_integer(unsigned(T0)) + to_integer(unsigned(T1) * 10) +
-							to_integer(unsigned(T2) * 100) + to_integer(unsigned(T3) * 1000) +
-							to_integer(unsigned(T4) * 10000) + to_integer(unsigned(T5) * 100000) +
-							to_integer(unsigned(T6) * 1000000));
-					O <= std_logic_vector(to_unsigned(Tint, NUMBITS));
+					Tint := (to_integer(unsigned(T0)) + to_integer(unsigned(T1)) * 10 +
+							to_integer(unsigned(T2)) * 100 + to_integer(unsigned(T3)) * 1000 +
+							to_integer(unsigned(T4)) * 10000 + to_integer(unsigned(T5)) * 100000 +
+							to_integer(unsigned(T6)) * 1000000);
+					O <= conv_std_logic_vector(Tint, NUMBITS);
 				
-					--O <= std_logic_vector(unsigned(T0) + (unsigned(T1) * 10) + 
-					--						(unsigned(T2) * 100) + (unsigned(T3) * 1000) + 
-					--						(unsigned(T4) * 10000) + (unsigned(T5) * 100000) + 
-					--						(unsigned(T6) * 1000000));
-				--if neg
 				else
-					Tint <= (to_integer(unsigned(T0)) + to_integer(unsigned(T1) * 10) +
-							to_integer(unsigned(T2) * 100) + to_integer(unsigned(T3) * 1000) +
-							to_integer(unsigned(T4) * 10000) + to_integer(unsigned(T5) * 100000) +
-							to_integer(unsigned(T6) * 1000000));
-					O <= not (std_logic_vector(to_unsigned(Tint, NUMBITS))) + 1;
+					Tint := (to_integer(unsigned(T0)) + to_integer(unsigned(T1)) * 10 +
+							to_integer(unsigned(T2)) * 100 + to_integer(unsigned(T3)) * 1000 +
+							to_integer(unsigned(T4)) * 10000 + to_integer(unsigned(T5)) * 100000 +
+							to_integer(unsigned(T6)) * 1000000);
+					O <= not (conv_std_logic_vector(Tint, NUMBITS)) + 1;
 				
-					--O <= not (std_logic_vector(unsigned(T0) + (unsigned(T1) * 10) + 
-					--						(unsigned(T2) * 100) + (unsigned(T3) * 1000) + 
-					--						(unsigned(T4) * 10000) + (unsigned(T5) * 100000) + 
-					--						(unsigned(T6) * 1000000))) + 1;
 				end if;
 			end if;
 	end process;	
