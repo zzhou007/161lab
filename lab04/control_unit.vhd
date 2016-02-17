d9bb5a8305c4e3cb961ec503a2262c7162fb135b
Zi Zhou <zzhou007@ucr.edu>
2016-02-16 21:08:20 -0800

    Update control_unit.vhd

--- lab04/control_unit.vhd
+++ lab04/control_unit.vhd
@@ -1,7 +1,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-use work.cpu_constant_library.all;
-
+ 
 entity control_unit is
   port (
     instr_op          : in std_logic_vector(5 downto 0);
@@ -15,58 +14,49 @@ entity control_unit is
     reg_write         : out std_logic
     );
 end control_unit;
-
+ 
 architecture Behavioral of control_unit is
 
 begin
-	process (instr_op)
-		begin
-		case instr_op is
-			when "000000" => --r format
-				reg_dst <= '1';
-				alu_src <= '0';
-				mem_to_reg <= '0';
-				reg_write <= '1';
-				mem_read <= '0';
-				mem_write <= '0';
-				branch <= '0';
-				alu_op <= "10";
-			when "100011" => -- lw
-				reg_dst <= '0';
-				alu_src <= '1';
-				mem_to_reg <= '1';
-				reg_write <= '1';
-				mem_read <= '1';
-				mem_write <= '0';
-				branch <= '0';
-				alu_op <= "00";
-			when "101011" => -- sw
-				--reg_dst <= '1';
-				alu_src <= '1';
-				--mem_to_reg <= '0';
-				reg_write <= '0';
-				mem_read <= '0';
-				mem_write <= '1';
-				branch <= '0';
-				alu_op <= "00";
-			when "000100" => -- beq
-				--reg_dst <= '1';
-				alu_src <= '0';
-				--mem_to_reg <= '0';
-				reg_write <= '0';
-				mem_read <= '0';
-				mem_write <= '0';
-				branch <= '1';
-				alu_op <= "01";
-			when others => -- bad input
-				reg_dst <= '0';
-				alu_src <= '0';
-				mem_to_reg <= '0';
-				reg_write <= '0';
-				mem_read <= '0';
-				mem_write <= '0';
-				branch <= '0';
-				alu_op <= "00";
-		end case;
-	end process;
-end Behavioral;
+    process(instr_op) begin
+        case instr_op is
+            when "000000" =>
+                reg_dst <= '1';
+                alu_src <= '0';
+                mem_to_reg <= '0';
+                reg_write <= '1';
+                mem_read <= '0';
+                mem_write <= '0';
+                branch <= '0';
+                alu_op <= "10";
+            when "100011" =>
+                reg_dst <= '0';
+                alu_src <= '1';
+                mem_to_reg <= '1';
+                reg_write <= '1';
+                mem_read <= '1';
+                mem_write <= '0';
+                branch <= '0';
+                alu_op <= "00";
+            when "101011" =>
+                --reg_dst <= '0';
+                alu_src <= '1';
+                --mem_to_reg <= '1';
+                reg_write <= '0';
+                mem_read <= '0';
+                mem_write <= '1';
+                branch <= '0';
+                alu_op <= "00";
+            when "000100" =>
+                --reg_dst <= '0';
+                alu_src <= '0';
+                --mem_to_reg <= '1';
+                reg_write <= '0';
+                mem_read <= '0';
+                mem_write <= '0';
+                branch <= '1';
+                alu_op <= "01";
+            when others =>
+        end case;
+    end process;
+end Behavioral; 
