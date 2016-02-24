6dc73efdc39d3f933b29f2940a49f1feb12ef5b0
Zi Zhou <zzhou007@ucr.edu>
2016-02-23 21:17:15 -0800

    Update cpu_components.vhd

--- lab05/cpu_components.vhd
+++ lab05/cpu_components.vhd
@@ -249,5 +249,110 @@ begin
     end if;
   end process;
 
+end Behavioral;
 
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.cpu_constant_library.all;
+
+entity alu_control is
+  port (
+    alu_op            : in std_logic_vector(1 downto 0);
+    instruction_5_0   : in std_logic_vector(5 downto 0);
+    alu_out           : out std_logic_vector(3 downto 0)
+    );
+end alu_control;
+
+architecture Behavioral of alu_control is
+
+begin
+
+  process (alu_op, instruction_5_0) 
+  begin
+    if alu_op = "00" then -- LW or SW
+      alu_out <= ALU_ADD;
+      
+    elsif alu_op = "01" then -- branch
+      alu_out <= ALU_SUBTRACT;
+      
+    else -- R_Type
+      case instruction_5_0 is
+        when FUNCT_AND       =>      alu_out <= ALU_AND;
+        when FUNCT_OR        =>      alu_out <= ALU_OR;
+        when FUNCT_ADD       =>      alu_out <= ALU_ADD;
+        when FUNCT_SUBTRACT  =>      alu_out <= ALU_SUBTRACT;
+        when FUNCT_LESS_THAN =>      alu_out <= ALU_LESS_THAN;
+        when FUNCT_NOR       =>      alu_out <= ALU_NOR;
+        when others          =>      alu_out <= (others => '0');
+      end case;
+    end if;
+  end process;
+
+end Behavioral;
+
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+-- ----- ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.cpu_constant_library.all;
+
+entity control_unit is
+  port (
+    instr_op           : in std_logic_vector(5 downto 0);
+    reg_dst           : out std_logic;
+    branch            : out std_logic;
+    mem_read          : out std_logic;
+    mem_to_reg        : out std_logic;
+    alu_op            : out std_logic_vector(1 downto 0);
+    mem_write         : out std_logic;
+    alu_src           : out std_logic;
+    reg_write         : out std_logic
+    );
+end control_unit;
+
+architecture Behavioral of control_unit is
+
+begin
+  
+  process (instr_op)
+  begin
+    reg_dst     <= '0';
+    branch      <= '0';
+    mem_read    <= '0';
+    mem_to_reg  <= '0';
+    alu_op      <= (others => '0');
+    mem_write   <= '0';
+    alu_src     <= '0';
+    reg_write   <= '0';
+    
+    case instr_op is
+      when OPCODE_R_TYPE =>         reg_dst    <= '1';
+                                    reg_write  <= '1';
+                                    alu_op     <= "10";
+      
+      when OPCODE_ADDI =>           alu_src    <= '1';
+                                    reg_write  <= '1';
+                                                
+      when OPCODE_LOAD_WORD =>      alu_src    <= '1';
+                                    mem_to_reg <= '1';
+                                    reg_write  <= '1';
+                                    mem_read   <= '1';
+                                        
+      when OPCODE_STORE_WORD =>     alu_src    <= '1';
+                                    mem_write  <= '1';
+                                        
+      when OPCODE_BRANCH_EQ =>      branch     <= '1';
+                                    alu_op     <= "01";
+                                        
+      when others =>
+        
+    end case;
+  end process;
+  
 end Behavioral;
